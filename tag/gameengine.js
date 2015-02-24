90ebb7034eb26619df7fbaeff10f17eb1b9313a4
Nick Duncan <nick.b.duncan@gmail.com>
2015-02-23 21:34:22 -0800

    Initial commit

---
+++ tag/gameengine.js
@@ -0,0 +1,179 @@
+// This game shell was happily copied from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (/* function */ callback, /* DOMElement */ element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+
+function Timer() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+}
+
+Timer.prototype.tick = function () {
+    var wallCurrent = Date.now();
+    var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
+
+    var gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
+}
+
+function GameEngine() {
+    this.entities = [];
+    this.pellets = [];
+    this.ghosts = [];
+    this.pelletSpawner = 0;
+    this.showOutlines = false;
+    this.ctx = null;
+    this.click = null;
+    this.mouse = null;
+    this.wheel = null;
+    this.surfaceWidth = null;
+    this.surfaceHeight = null;
+}
+
+GameEngine.prototype.init = function (ctx) {
+    this.ctx = ctx;
+    this.surfaceWidth = this.ctx.canvas.width;
+    this.surfaceHeight = this.ctx.canvas.height;
+    this.timer = new Timer();
+    console.log('game initialized');
+};
+
+GameEngine.prototype.start = function () {
+    console.log("starting game");
+    var that = this;
+    (function gameLoop() {
+        that.loop();
+        requestAnimFrame(gameLoop, that.ctx.canvas);
+    })();
+};
+
+GameEngine.prototype.addPacman = function (entity) {
+    console.log('added entity');
+    this.entities.push(entity);
+};
+
+GameEngine.prototype.addGhost = function (entity) {
+    this.ghosts.push(entity);
+}
+
+GameEngine.prototype.addPellet = function (pellet) {
+    this.pellets.push(pellet);
+};
+
+GameEngine.prototype.draw = function () {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.ctx.save();
+    for (var i = 0; i < this.entities.length; i++) {
+        this.entities[i].draw(this.ctx);
+    }
+    for (var i = 0; i < this.ghosts.length; i++) {
+        this.ghosts[i].draw(this.ctx);
+    }
+    var pelletsCount = this.pellets.length;
+    for (var i = 0; i < pelletsCount; i++) {
+        if (!this.pellets[i].removeFromWorld) {
+            this.pellets[i].draw(this.ctx);
+        }
+    }
+    this.ctx.restore();
+};
+
+GameEngine.prototype.update = function () {
+    var entitiesCount = this.entities.length;
+    this.pelletSpawner += 5;
+    if (this.pelletSpawner >= 10000) {
+        for (var i = 0; i < 5; i++) {
+            var pellet = new PowerPellet(this);
+            this.addPellet(pellet);
+        }
+        this.pelletSpawner = 0;
+    }
+    for (var i = 0; i < entitiesCount; i++) {
+        var entity = this.entities[i];
+
+        if (!entity.removeFromWorld) {
+            entity.update();
+        }
+    }
+
+    for (var i = 0; i < this.ghosts.length; i++) {
+        var ghost = this.ghosts[i];
+        if (!ghost.removeFromWorld) {
+            ghost.update();
+        }
+    }
+    for (var i = this.entities.length - 1; i >= 0; --i) {
+        if (this.entities[i].removeFromWorld) {
+            this.entities.splice(i, 1);
+        }
+    }
+
+    for(var i = this.ghosts.length - 1; i >= 0; --i) {
+        if (this.ghosts[i].removeFromWorld) {
+            this.ghosts.splice(i, 1);
+        }
+    }
+    for (var i = this.pellets.length -1; i >= 0; --i) {
+        if (this.pellets[i].removeFromWorld) {
+            this.pellets.splice(i, 1);
+        }
+    }
+};
+
+GameEngine.prototype.loop = function () {
+    this.clockTick = this.timer.tick();
+    this.update();
+    this.draw();
+    this.click = null;
+    this.rightclick = null;
+    this.wheel = null;
+}
+
+function Entity(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.removeFromWorld = false;
+}
+
+Entity.prototype.update = function () {
+};
+
+Entity.prototype.draw = function (ctx) {
+    if (this.game.showOutlines && this.radius) {
+        this.game.ctx.beginPath();
+        this.game.ctx.strokeStyle = "green";
+        this.game.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.game.ctx.stroke();
+        this.game.ctx.closePath();
+    }
+};
+
+Entity.prototype.rotateAndCache = function (image, angle) {
+    var offscreenCanvas = document.createElement('canvas');
+    var size = Math.max(image.width, image.height);
+    offscreenCanvas.width = size;
+    offscreenCanvas.height = size;
+    var offscreenCtx = offscreenCanvas.getContext('2d');
+    offscreenCtx.save();
+    offscreenCtx.translate(size / 2, size / 2);
+    offscreenCtx.rotate(angle);
+    offscreenCtx.translate(0, 0);
+    offscreenCtx.drawImage(image, -(image.width / 2), -(image.height / 2));
+    offscreenCtx.restore();
+    //offscreenCtx.strokeStyle = "red";
+    //offscreenCtx.strokeRect(0,0,size,size);
+    return offscreenCanvas;
+}
